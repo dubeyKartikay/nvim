dbf93c55a04874ddc040ca18689ca8a4da0fe161
Kartikay Dubey <kartikay21326@iiitd.ac.in>
2024-12-31 09:26:08 -0800

    remote

--- lua/plugins/copilot-chat.lua
+++ lua/plugins/copilot-chat.lua
@@ -1,15 +1,16 @@
 return {
   {
     "CopilotC-Nvim/CopilotChat.nvim",
-    event="VeryLazy",
     dependencies = {
       { "zbirenbaum/copilot.lua" }, -- or zbirenbaum/copilot.lua
       { "nvim-lua/plenary.nvim", branch = "master" }, -- for curl, log and async functions
     },
     build = "make tiktoken", -- Only on MacOS or Linux
-    opts = {
-      -- See Configuration section for options
+    cmd = {
+      "CopilotChat",
+      "CopilotChatStart",
+      "CopilotChatStop",
+      "CopilotChatToggle"
     },
-    -- See Commands section for default commands if you want to lazy load on them
   },
 }
