57ef7a0ae14efeb84e8bd49d0662249011851f43
Kartikay Dubey <kartikay21326@iiitd.ac.in>
2024-12-30 22:24:10 -0800

    ai2

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,52 +1,28 @@
 return {
-  "zbirenbaum/copilot.lua",
-  cmd = "Copilot",
-  event = "InsertEnter",
-  config = function()
-    require("copilot").setup {
-      panel = {
-        enabled = true,
-        auto_refresh = false,
-        keymap = {
-          jump_prev = "[[",
-          jump_next = "]]",
-          accept = "<CR>",
-          refresh = "gr",
-          open = "<M-CR>",
+  {
+    "zbirenbaum/copilot.lua",
+    cmd = "Copilot",
+    event = "InsertEnter",
+    config = function()
+      require("copilot").setup {
+        panel = {
+          enable = false,
         },
-        layout = {
-          position = "bottom", -- | top | left | right | horizontal | vertical
-          ratio = 0.4,
+        suggestion = {
+          enable = false,
         },
-      },
-      suggestion = {
-        enabled = true,
-        auto_trigger = true,
-        hide_during_completion = true,
-        debounce = 75,
-        keymap = {
-          accept = "<M-l>",
-          accept_word = false,
-          accept_line = false,
-          next = "<M-]>",
-          prev = "<M-[>",
-          dismiss = "<C-]>",
+        filetypes = {
+          ["*"] = true,
         },
-      },
-      filetypes = {
-        yaml = false,
-        markdown = false,
-        help = false,
-        gitcommit = false,
-        gitrebase = false,
-        hgcommit = false,
-        svn = false,
-        cvs = false,
-        ["."] = false,
-      },
-      copilot_node_command = "node", -- Node.js version must be > 18.x
-      server_opts_overrides = {},
-    }
-  end,
-  opts = {},
+      }
+    end,
+    opts = {},
+  },
+  {
+    "zbirenbaum/copilot-cmp",
+    event = "VeryLazy",
+    config = function()
+      require("copilot_cmp").setup()
+    end,
+  },
 }
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,13 +1,13 @@
 return {
   {
     "stevearc/conform.nvim",
-    event = 'BufWritePre', -- uncomment for format on save
+    event = "BufWritePre", -- uncomment for format on save
     opts = require "configs.conform",
   },
 
   {
     "nvim-tree/nvim-tree.lua",
-    enabled = false
+    enabled = false,
   },
 
   -- These are some examples, uncomment them if you want to see them work!
@@ -19,12 +19,76 @@ return {
   },
 
   {
-  	"nvim-treesitter/nvim-treesitter",
-  	opts = {
-  		ensure_installed = {
-  			"vim", "lua", "vimdoc",
-       "html", "css", "cpp", "python","typescript","java"
-  		},
-  	},
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "vim",
+        "lua",
+        "vimdoc",
+        "html",
+        "css",
+        "cpp",
+        "python",
+        "typescript",
+        "java",
+      },
+    },
+  },
+  {
+    "hrsh7th/nvim-cmp",
+    opts = function(_, conf)
+      local kind_icons = {
+        Text = "󰦨",
+        Method = "",
+        Function = "󰊕",
+        Constructor = "",
+        Field = "",
+        Variable = "",
+        Class = "",
+        Interface = "",
+        Module = "",
+        Property = "",
+        Unit = "",
+        Value = "",
+        Enum = "",
+        Keyword = "",
+        Snippet = "",
+        Color = "",
+        File = "",
+        Reference = "",
+        Folder = "",
+        EnumMember = "",
+        Constant = "",
+        Struct = "",
+        Event = "",
+        Operator = "",
+        TypeParameter = "",
+        Copilot = "",
+      }
+      conf.sources = {
+        { name = "copilot"},
+        { name = "nvim_lsp" },
+        { name = "luasnip" },
+        { name = "buffer" },
+        { name = "nvim_lua" },
+        { name = "path" },
+      }
+
+      conf.formatting = {
+        fields = { "kind", "abbr", "menu" },
+        format = function(entry, vim_item)
+          -- Kind icons
+          vim_item.kind = string.format("%s", kind_icons[vim_item.kind])
+          vim_item.menu = ({
+            nvim_lsp = "[LSP]",
+            luasnip = "[Snippet]",
+            buffer = "[Buffer]",
+            path = "[Path]",
+            copilot = "[Copilot]",
+          })[entry.source.name]
+          return vim_item
+        end,
+      }
+    end,
   },
 }
