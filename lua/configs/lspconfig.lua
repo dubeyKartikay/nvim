6eaa637506804ef66f162cc9a77b567496062524
Kartikay Dubey <kartikay21326@iiitd.ac.in>
2025-01-02 12:00:08 -0800

    java

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -4,7 +4,7 @@ require("nvchad.configs.lspconfig").defaults()
 local lspconfig = require "lspconfig"
 
 -- EXAMPLE
-local servers = { "html", "cssls", "ts_ls","clangd","tailwindcss", "rust_analyzer", "pyright"}
+local servers = { "html", "cssls", "ts_ls","clangd","tailwindcss", "rust_analyzer", "pyright", "jdtls"}
 local nvlsp = require "nvchad.configs.lspconfig"
 
 -- lsps with default config
